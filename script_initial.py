153486b36583bf170acb2bc3a0b686e00f5e6f3a
PFALima <6286030@bdeb.qc.ca>
2024-07-17 15:57:35 -0400

    Version 3 - Add Object Personne2

--- script_initial.py
+++ script_initial.py
@@ -5,12 +5,14 @@ class Personne:
     def __init__(self,nom, prenom, age) : 
         self.nom    = nom
         self.prenom = prenom
-        self.age    = age
+        self.age    = age 
     #définition de la méthode se_présente
     def se_presente(self):
         print(f"je m'appelle {self.nom}, {self.prenom} et j'ai {self.age} ans.")
 
  # Criação de uma instancia da Classe Personne
 personne1=Personne("Lunaria", "Luna", 34)
+
+personne2=Personne("Solaria", "Sol", 34)
 
 personne1.se_presente()
